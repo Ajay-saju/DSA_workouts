7b85576de9ede97acd69d5d7d0d40b408121706c
Ajay-saju <ajaysaju987@gmail.com>
2023-09-28 14:30:46 +0530

    for loop

---
+++ Topics/Introduction to Java/Scaler Workouts/For loop/is_it_prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class is_it_prime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+            if (count > 2) {
+                break;
+            }
+        }
+        if (count > 2) {
+            System.out.print("NO");
+        } else {
+            System.out.print("YES");
+        }
+
+    }
+}
